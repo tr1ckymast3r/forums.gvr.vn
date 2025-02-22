3b0844a6672c0259e77821d0b0f42830b04f9c50
Tr1cky <tricky1786@gmail.com>
2025-02-23 03:47:16 +0700

    =Init

--- scripts/seed-data.js
+++ scripts/seed-data.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Fallback to empty string if env vars are not defined
-const supabaseUrl = process.env.SUPABASE_URL || 'https://qfviromhazkxwfymgqsd.supabase.co';
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmdmlyb21oYXpreHdmeW1ncXNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwMjE1NjYsImV4cCI6MjA1NTU5NzU2Nn0.n2nwLN-5TmyALP7gdE4O9A5a5blA1b3EHwLUmhrTRdQ';
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || '';
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
